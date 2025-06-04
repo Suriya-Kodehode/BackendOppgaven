715eca9cf1ef9b6ca9bd25fcd4552c466e86c780
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-06-04 17:04:55 +0200

    comment for .env template

--- sequelize.js
+++ sequelize.js
@@ -2,6 +2,15 @@ import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
 config();
 
+// In .env file:
+
+// DB_HOST=""
+// DB_NAME=""
+// DB_USER=""
+// DB_PASSWORD=""
+// DB_PORT=""
+// JWT_SECRET=""
+
 const db = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
